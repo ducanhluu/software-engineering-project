c323bc042df5cc9d7dbb7a662a35e183610bca33
Duc-Anh Luu <Duc-Anh.Luu@grenoble-inp.org>
2017-01-15 21:17:59 +0100

    mis a jour Modulo et les tests interactives

--- src/main/java/fr/ensimag/deca/tree/Modulo.java
+++ src/main/java/fr/ensimag/deca/tree/Modulo.java
@@ -2,6 +2,7 @@ package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.context.Type;
 import fr.ensimag.deca.DecacCompiler;
+import static fr.ensimag.deca.codegen.MemoryManagement.divisionIsUsed;
 import static fr.ensimag.deca.codegen.MemoryManagement.getAvailableRegister;
 import static fr.ensimag.deca.codegen.MemoryManagement.getLastUsedRegisterToStore;
 import static fr.ensimag.deca.codegen.MemoryManagement.setLastUsedRegister;
@@ -11,6 +12,9 @@ import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.ima.pseudocode.GPRegister;
 import fr.ensimag.ima.pseudocode.instructions.LOAD;
 import fr.ensimag.deca.context.TypeDefinition;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.instructions.BEQ;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
 import fr.ensimag.ima.pseudocode.instructions.REM;
 
 /**
@@ -27,37 +31,44 @@ public class Modulo extends AbstractOpArith {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        Type type1=this.getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
-        Type type2=this.getRightOperand().verifyExpr(compiler, localEnv, currentClass);
-        if ( !type1.isInt() || !type2.isInt()){
-            throw new ContextualError("you can apply % only on integers",this.getLocation());
+        Type type1 = this.getLeftOperand().verifyExpr(compiler, localEnv, currentClass);
+        Type type2 = this.getRightOperand().verifyExpr(compiler, localEnv, currentClass);
+        if (!type1.isInt() || !type2.isInt()) {
+            throw new ContextualError("you can apply % only on integers", this.getLocation());
         }
         this.getLeftOperand().setType(type1);
         this.getRightOperand().setType(type2);
-        TypeDefinition typeDef=compiler.getEnvType().get(compiler.getEnvType().getDict().create("int"));
+        TypeDefinition typeDef = compiler.getEnvType().get(compiler.getEnvType().getDict().create("int"));
         this.setType(typeDef.getType());
-        return typeDef.getType();   
+        return typeDef.getType();
     }
-
 
     @Override
     protected String getOperatorName() {
         return "%";
     }
-    
+
     @Override
     protected void codeGenInst(DecacCompiler compiler) {
         super.codeGenInst(compiler);
-        
+        divisionIsUsed = true;
+        compiler.addInstruction(new CMP(0, reg));
+        compiler.addInstruction(new BEQ(new Label("division_by_zero_error")));
+
         if (getRightOperand() instanceof Identifier && !(getLeftOperand() instanceof Identifier)) {
+            compiler.addInstruction(new CMP(0, reg));
+            compiler.addInstruction(new BEQ(new Label("division_by_zero_error")));
             compiler.addInstruction(new LOAD(((Identifier) getRightOperand()).getVariableDefinition().getOperand(), getAvailableRegister(compiler)));
             GPRegister reg2 = getLastUsedRegisterToStore();
             compiler.addInstruction(new REM(reg, reg2));
             setLastUsedRegister(reg2.getNumber());
         } else {
+            compiler.addInstruction(new LOAD(val, getAvailableRegister(compiler)));
+            compiler.addInstruction(new CMP(0, getLastUsedRegisterToStore()));
+            compiler.addInstruction(new BEQ(new Label("division_by_zero_error")));
             compiler.addInstruction(new REM(val, reg));
             setLastUsedRegister(reg.getNumber());
         }
     }
-    
+
 }
