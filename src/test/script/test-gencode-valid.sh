6f62b4904f62b7ed9502b64ee7aacd6fa80e97e2
otmane farid <othmane.farid@ensimag.grenoble-inp.fr>
2017-01-16 15:16:01 +0100

    generation automatique du code de notre base de test

--- src/test/script/test-gencode-valid.sh
+++ src/test/script/test-gencode-valid.sh
@@ -1,17 +1,17 @@
 #!/bin/sh 
 cd "$(dirname "$0")"/../../.. || exit 1
 
-PATH=./src/test/script/launchers/:"$PATH"
-
-for i in ./src/test/deca/codegen/valid/notprovided/*.deca
+PATH=./src/test/script/launchers/:./src/main/bin"$PATH"
+rm -f ./src/test/deca/etapeC/valide/*.ass 2>/dev/null
+for i in ./src/test/deca/etapeC/valide/*.deca
 do
-    if  test_codegen "$i" 2>&1 | grep -q -e './src/test/deca'
-    then
-	test_codegen "$i" >&  "${i%.deca}".res
-	echo "Echec inattendu pour le test ""${i#*notprovided/}" "du test_context"
+    decac "$i"|| exit 1
+    if[! - f  "${i%.deca}".ass];then
+	
+	echo "Fichier" "${i#*valide/}"".ass" "non généré."
 	echo 1
     else
-	echo "Succes attendu du test ""${i#*notprovided/}" "du test_context"
+	echo "Fichier" "${i#*valide/}"".ass" "est généré."
     fi
     
     
