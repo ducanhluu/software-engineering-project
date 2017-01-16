b53ed97d579031cc1bb486ce15228851914d8d43
otmane farid <othmane.farid@ensimag.grenoble-inp.fr>
2017-01-16 11:57:37 +0100

    automatisation du test-gencode-valide

--- src/test/script/test-gencode-valid.sh
+++ src/test/script/test-gencode-valid.sh
@@ -1,17 +1,33 @@
 #!/bin/sh 
 cd "$(dirname "$0")"/../../.. || exit 1
+noir='\e[0;30m'
+gris='\e[1;30m'
+rougefonce='\e[0;31m'
+rose='\e[1;31m'
+vertfonce='\e[0;32m'
+vertclair='\e[1;32m'
+orange='\e[0;33m'
+jaune='\e[1;33m'
+bleufonce='\e[0;34m'
+bleuclair='\e[1;34m'
+violetfonce='\e[0;35m'
+violetclair='\e[1;35m'
+cyanfonce='\e[0;36m'
+cyanclair='\e[1;36m'
+grisclair='\e[0;37m'
+blanc='\e[1;37m'
 
-PATH=./src/test/script/launchers/:"$PATH"
-
-for i in ./src/test/deca/codegen/valid/notprovided/*.deca
+neutre='\e[0;m'
+PATH=./src/test/script/launchers/:./src/main/bin:"$PATH"
+rm -f ./src/test/deca/etapeC/valid/*.ass 2>/dev/null
+for i in ./src/test/deca/etapeC/valid/*.deca
 do
-    if  test_codegen "$i" 2>&1 | grep -q -e './src/test/deca'
-    then
-	test_codegen "$i" >&  "${i%.deca}".res
-	echo "Echec inattendu pour le test ""${i#*notprovided/}" "du test_context"
-	echo 1
+    decac "$i"
+    if  [ ! -f "${i%.deca}".ass ];then 
+	echo -e "${rougefonce}Fail${neutre}" "Fichier ""${i%.deca}"".ass" "non généré"
+	exit 1
     else
-	echo "Succes attendu du test ""${i#*notprovided/}" "du test_context"
+	echo -e "${vertclair}Pass${neutre}" "Fichier ""${i%.deca}"".ass" "généré"
     fi
     
     
