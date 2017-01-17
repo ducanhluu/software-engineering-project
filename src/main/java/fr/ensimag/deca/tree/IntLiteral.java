6832a69fd51b5447281d6c837432a8fb7592b044
Sami CHAKIR <SAMI.CHAKIR@ensimag.grenoble-inp.fr>
2017-01-17 16:39:30 +0100

    reglage du probleme de decoration de if

--- src/main/java/fr/ensimag/deca/tree/IntLiteral.java
+++ src/main/java/fr/ensimag/deca/tree/IntLiteral.java
@@ -37,7 +37,7 @@ public class IntLiteral extends AbstractExpr {
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
         TypeDefinition typeDef=compiler.getEnvType().get(compiler.getEnvType().getDict().create("int"));
-       
+        this.setType(typeDef.getType());
         return typeDef.getType();
     }
 
