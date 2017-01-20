a320dcc7ded697ed8ac93ed2ab5de8391f3e998f
Sami CHAKIR <SAMI.CHAKIR@ensimag.grenoble-inp.fr>
2017-01-20 14:40:13 +0100

    ajout de dot.java et return.java

---
+++ src/main/java/fr/ensimag/deca/tree/Return.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ensimag.deca.tree;
+
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.context.ClassDefinition;
+import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.tools.IndentPrintStream;
+import java.io.PrintStream;
+import org.apache.commons.lang.Validate;
+
+/**
+ *
+ * @author chakirs
+ */
+public class Return extends AbstractInst {
+    private AbstractExpr operand;
+    public AbstractExpr getOperand(){
+        return this.operand;
+    }
+    public Return(AbstractExpr operand){
+        Validate.notNull(operand);
+        this.operand=operand;
+    }
+    @Override
+    protected void verifyInst(DecacCompiler compiler, EnvironmentExp localEnv, ClassDefinition currentClass, Type returnType) throws ContextualError {
+        this.operand.verifyRValue(compiler, localEnv, currentClass, returnType);
+        
+    }
+
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void decompile(IndentPrintStream s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void prettyPrintChildren(PrintStream s, String prefix) {
+        operand.prettyPrint(s, prefix, true);
+    }
+
+    @Override
+    protected void iterChildren(TreeFunction f) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
